27c6083318e50ead8271c39e9107d6534ff32894
Jasper Rodda <jasper475@gmail.com>
2024-02-03 13:14:44 -0600

    Substraction  addition.py

--- src/addition.py
+++ src/addition.py
@@ -7,7 +7,13 @@
 # This is 6th commit by Jasper - Enabled GitHub Actions 
 def add(a, b):
     return a + b
-
+def sub(a, b):
+    return a - b
+    
+def test_sub():
+    assert sub(3,1) ==2
+    assert sub (1,1)==0
+    
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
